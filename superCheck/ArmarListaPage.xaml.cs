800225c4765550544bde8ce6ec6477535b781fb2
Maxi <maxi.ottino@gmail.com>
2018-05-17 23:42:39 -0300

    Primer pantalla de inicio con dos botones y un evento para ir a otro Layout.

---
+++ superCheck/ArmarListaPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace superCheck
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ArmarListaPage : ContentPage
+	{
+		public ArmarListaPage ()
+		{
+            var lblPrueba = new Label
+            {
+                Text = "Cliqueaste en ARMAR LISTA"
+
+            };
+
+            Content = new StackLayout
+            {
+
+                Children = { lblPrueba }
+            };
+		}
+	}
+}
--- superCheck/MainPage.xaml.cs
+++ superCheck/MainPage.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Android;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -11,7 +13,30 @@ namespace superCheck
 	{
 		public MainPage()
 		{
-			InitializeComponent();
+            //InitializeComponent();
+            var btnArmarLista = new Button
+            {
+                Text = "ARMAR LISTA"
+            };
+
+            btnArmarLista.Clicked += (sender, e) =>
+            {
+                this.Navigation.PushModalAsync(new ArmarListaPage());
+                //http://www.heroesdelcodigo.com/xamarin-forms-navegacion/
+            };
+           
+            var btnVerListas = new Button
+            {
+                Text = "VER MIS LISTAS"
+            };
+
+
+            Content = new StackLayout
+            {
+                Padding = 30,
+                Spacing = 10,
+                Children = { btnArmarLista , btnVerListas }
+            };
 		}
 	}
 }
